c6ab6065d16dd4649e1e09788c5cd1b193c9bb55
Emre Duman <76225090+Emre1Duman@users.noreply.github.com>
2023-10-13 22:19:47 +0100

    dictionaries of different monsters

---
+++ monsters.py
@@ -0,0 +1,62 @@
+
+basic_monsters = [
+    {
+        "name": "The Demogorgon",
+        "health": 100
+    },
+    {
+        "name": "Mimic",
+        "health": 100
+    },
+    {
+        "name": "Vampire",
+        "health": 100
+    },
+    {
+        "name": "Lycanthropes",
+        "health": 100
+    },
+    {
+        "name": "Worm",
+        "health": 100
+    },
+    {
+        "name": "Vampire",
+        "health": 100
+    },
+    {
+        "name": "Ogre",
+        "health": 100
+    },
+    {
+        "name": "Goblin",
+        "health": 100
+    },
+]
+
+mini_boss_monsters = [
+    {
+        "name": "Troll",
+        "health": 200
+    },
+    {
+        "name": "Giant",
+        "health": 200
+    },
+]
+
+boss_monsters = [
+    {
+        "name": "Kraken",
+        "health": 400
+    },
+    {
+        "name": "Dragon",
+        "health": 400
+    },
+    {
+        "name": "cthulhu",
+        "health": 600
+    },
+]
+
