4092ea5a143a5863c7f580dbd0affc5e49fde7cd
Emre Duman <76225090+Emre1Duman@users.noreply.github.com>
2023-10-18 20:27:57 +0100

    updated

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from characters import heros
 
-print("Adventure awaits! Are you ready?")
+member0 = heros[0]["name"]
+member1 = heros[1]["name"]
+member2 = heros[2]["name"]
+
+start = input("Adventure awaits! Are you ready? Type 'y' or 'n'. ").lower()
+
+if start == "y":
+    print(f"You and the members of your team, {member1} & {member2}, wake up in the middle of the forest")
+    level_1 =input("You look around and see a path on the left leading to a castle, and a massive cave entrance to the right, Go 'left' or 'right'? ")
